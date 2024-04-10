1e08d08f1ed3956d5a5cd7b3e5802edd11e57640
Claudia F <131504107+clawddee@users.noreply.github.com>
2024-04-10 19:22:08 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,22 @@
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+}
+
+// Make sure the first slide is shown when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+  showSlides(slideIndex);
+});

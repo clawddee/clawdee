963bd1edca8a750c874cd093381001bc566f9f90
Claudia F <131504107+clawddee@users.noreply.github.com>
2024-04-10 21:11:41 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -16,12 +16,12 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
-// Make sure the first slide is shown when the page loads
+
 document.addEventListener("DOMContentLoaded", function() {
   showSlides(slideIndex);
 });
 
 .gradient-button.clicked {
-  /* Example: Change gradient colors */
+ 
   background-image: linear-gradient(to right, #6a11cb 0%, #2575fc 100%);
 }

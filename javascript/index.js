3ceb6d7488466343f8edc430353e64bafc72ed40
Claudia F <131504107+clawddee@users.noreply.github.com>
2024-06-26 20:54:07 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -8,7 +8,7 @@ function plusSlides(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}sss
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
